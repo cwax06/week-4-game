00f080a566e668ee8bd6c1117b139b299be38f03
Andrew Jones <Andrew@AHJones-iMac.local>
2017-10-10 21:50:00 -0600

    Crystal Variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+// GLOBAL VARIABLES
+// =======================================================
+
+// Crystal Variables
+var crystal = {
+    blue:
+    {
+        name: "blue",
+        value: 0
+    },
+    green:
+    {
+        name: "green",
+        value: 0
+    },
+    red:
+    {
+        name: "red",
+        value: 0
+    },
+    yellow:
+    {
+        name: "yellow",
+        value: 0
+    }
+};
+
+// Scores (Current and Target)
+var currentScore = 0;
+var targetScore = 0;
+
+// Wins and Losses
+var winCount = 0;
+var lossCount = 0;
+
+// FUNCTIONS
+// ======================================================
+
+// Helper Function for getting random numbers
+var getRandom = function (min, max) {
+
+};
+
+// Starts the Game (and restarts the game)
+var startGame = function () {
+
+    // Reset the Current Score
+
+    // Set a new Target Score (between 19 and 120)
+    var targetScore = getRandom(19, 120)
+
+    // Set different values for each of the crystals (between 1 and 12)
+
+    // Change the HTML to reflect all of these changes
+
+    // Testing Console
+    console.log(getRandom)
+    console.log(targetScore)
+};
